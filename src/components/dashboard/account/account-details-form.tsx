9151cdb17dc5b4b84278ede86650edc43d13c8ed
FunkyGary <gary80212@gmail.com>
2024-04-18 13:28:04 +0800

    feat(dashboard/account): update api key and secret

--- src/components/dashboard/account/account-details-form.tsx
+++ src/components/dashboard/account/account-details-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -10,6 +11,7 @@ import Divider from '@mui/material/Divider';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
+import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 
 export function AccountDetailsForm(): React.JSX.Element {
@@ -36,6 +38,82 @@ export function AccountDetailsForm(): React.JSX.Element {
                 <OutlinedInput defaultValue="sofia@devias.io" label="Email address" name="email" disabled />
               </FormControl>
             </Grid>
+            <Grid xs={12}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: { xs: 250, md: 150 },
+                }}
+                alt="Binance"
+                src="/assets/Binance.png"
+              />
+            </Grid>
+            <Grid md={6} xs={12}>
+              <FormControl fullWidth required>
+                <InputLabel>Binance API key</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="Binance API key" />
+              </FormControl>
+            </Grid>
+            <Grid md={6} xs={12}>
+              <FormControl fullWidth required>
+                <InputLabel>Binance secret</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="Binance secret" />
+              </FormControl>
+            </Grid>
+            <Grid xs={12}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: { xs: 250, md: 150 },
+                }}
+                alt="OKX"
+                src="/assets/OKX.svg"
+              />
+            </Grid>
+            <Grid md={6} xs={12}>
+              <FormControl fullWidth required>
+                <InputLabel>OKX API key</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="OKX API key" />
+              </FormControl>
+            </Grid>
+            <Grid md={6} xs={12}>
+              <FormControl fullWidth required>
+                <InputLabel>OKX secret</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="OKX secret" />
+              </FormControl>
+            </Grid>
+            <Grid xs={12}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: { xs: 250, md: 150 },
+                }}
+                alt="max exchange"
+                src="/assets/max-exchange-logo.png"
+              />
+            </Grid>
+            <Grid md={6} xs={12}>
+              <FormControl fullWidth required>
+                <InputLabel>Max API key</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="Max API key" />
+              </FormControl>
+            </Grid>
+            <Grid md={6} xs={12}>
+              <FormControl fullWidth required>
+                <InputLabel>Max secret</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="Max secret" />
+              </FormControl>
+            </Grid>
+            <Grid xs={12}>
+              <Box
+                component="img"
+                sx={{
+                  maxWidth: { xs: 250, md: 150 },
+                }}
+                alt="Bitinex"
+                src="/assets/Bitinex.png"
+              />
+            </Grid>
             <Grid md={6} xs={12}>
               <FormControl fullWidth required>
                 <InputLabel>Bitfinex API key</InputLabel>
@@ -44,15 +122,15 @@ export function AccountDetailsForm(): React.JSX.Element {
             </Grid>
             <Grid md={6} xs={12}>
               <FormControl fullWidth required>
-                <InputLabel>Bitfinex API key</InputLabel>
-                <OutlinedInput defaultValue="geij1=i1" label="Binance API key" />
+                <InputLabel>Bitfinex secret</InputLabel>
+                <OutlinedInput defaultValue="geij1=i1" label="Bitfinex secret" />
               </FormControl>
             </Grid>
           </Grid>
         </CardContent>
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">Save details</Button>
+          <Button variant="contained">Save</Button>
         </CardActions>
       </Card>
     </form>
